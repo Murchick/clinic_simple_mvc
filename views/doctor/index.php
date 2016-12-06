e6ae543a9c329a990cf2c8f60f1917884c5fafb2
Murchik <murkovich.bogdan@gmail.com>
2016-12-06 17:11:12 +0200

    some fix

--- views/doctor/index.php
+++ views/doctor/index.php
@@ -1,18 +1,20 @@
 <div class="content">
 	<div class="pacients">
 	<p>All pacients</p>
-<table style="width:100%">
-  <tr>
-    <th>ID</th>
-    <th>Name</th>
-  </tr>
+
 <?php
 // var_dump($this->pacients);
-foreach ($this->pacients as $key => $value) {
-	echo "<tr><td>".$value[0]."</td><td><a href='".SITE_PATH."pacient/getdoctors/".$value[0]."'>".$value[1]."</a></td></tr><tr>";
+if (empty($this->pacients)) {
+	echo "Doctoh not has any pacient";
+} else {
+
+	echo '<table style="width:100%"><tr><th>ID</th><th>Name</th></tr>';
+	foreach ($this->pacients as $key => $value) {
+		echo "<tr><td>".$value[0]."</td><td><a href='".SITE_PATH."pacient/getdoctors/".$value[0]."'>".$value[1]."</a></td></tr><tr>";
+	}
+	echo "</table>";
 }
 ?>
-</table>
 </div>
 
 </div>
